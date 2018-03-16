36894f9839c077d3522f47c3e15822b2a17e392a
Marat Akhmetianov <marat.akhmetianov@gmail.com>
2018-03-16 17:10:00 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
+
+apt install -y git
 git clone -b monolith https://github.com/express42/reddit.git
-sleep 3
+  if [ $? -eq 0 ]; then
+    echo "Command executed successfully!"
+  else
+    echo "Command git clone completed with error. FAIL!!!"
+    exit 1
+  fi
+
 cd reddit && bundle install
-sleep 15
-sudo puma –d
+  if [ $? -eq 0 ]; then
+    echo "Command executed successfully!"
+  else
+    echo "Command bundle install completed with error. FAIL!!!"
+    exit 1
+  fi
+
+puma -d
+  if [ $? -eq 0 ]; then
+    echo "Command executed successfully!"
+  else
+    echo "Command puma -d completed with error. FAIL!!!"
+    exit 1
+  fi
+
+ps aux |grep -v grep | grep -q puma
+  if [ $? -eq 0 ]; then
+    echo "Puma started successfully"
+    exit 0
+  else
+    echo "Puma does not start FAIL!!!"
+    exit 1
+  fi
